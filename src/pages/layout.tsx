20b1ba39f42bd09bc7b4aeaf638c0fc9b7f80d5e
ZeeFuzooly <ziranfuzooly@gmail.com>
2024-08-01 08:00:48 +0530

    fixing build failures

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,12 +1,9 @@
 import { MantineProvider } from '@mantine/core';
-import { MantineThemeProvider } from '@mantine/styles';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <MantineProvider>
-      <MantineThemeProvider>
-        {children}
-      </MantineThemeProvider>
+      {children}
     </MantineProvider>
   );
 }
